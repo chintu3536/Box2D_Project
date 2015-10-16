ea2c731519a9f0a58a0d4ed44fc8b9b9293ba670
Chaitanya Rajesh <9948bajk@gmail.com>
2015-10-16 19:42:33 +0530

    Hydraulic System added

--- cs251_base_code/src/dominos.cpp
+++ cs251_base_code/src/dominos.cpp
@@ -46,87 +46,61 @@ namespace cs251
   dominos_t::dominos_t()
   {
     {
-        float32 rc=r*1.45f;
-        float32 xc=r*-20.0f;
-        float32 yc=r*7.5f;
-
-        b2BodyDef cardf;
-        cardf.type = b2_dynamicBody;
-        cardf.position.Set(x+rc*xc, y+rc*yc);
-        b2Body* car = m_world->CreateBody(&cardf);
-        car->ApplyLinearImpulse(b2Vec2(10,0), car->GetPosition() ,true);
-        b2FixtureDef cfd;
-        cfd.density = 5.0f;
-        cfd.friction = 0.0f;
-        b2CircleShape circle;
-        b2PolygonShape shape1;
-        //windows
-        shape1.SetAsBox(rc*1.0f, rc*1.0f, rc*b2Vec2(-1.8f,0.5f), 0);
-        cfd.shape = &shape1;
-        (car->CreateFixture(&cfd))->SetFcolor(0.5294*255,0.8078*255,0.9804*255,1.0);
-        shape1.SetAsBox(rc*1.0f, rc*1.0f, rc*b2Vec2(1.8f,0.5f), 0);
-        cfd.shape = &shape1;
-        (car->CreateFixture(&cfd))->SetFcolor(0.5294*255,0.8078*255,0.9804*255,1.0);
-        //body
-        shape1.SetAsBox(rc*2.5f, rc*2.5f);
-        cfd.shape = &shape1;      
-        (car->CreateFixture(&cfd))->SetFcolor(255,0,0,1.0,0);
-        shape1.SetAsBox(rc*6.25f, rc*1.25f, rc*b2Vec2(0.0f,-1.25f), 0);
-        cfd.shape = &shape1;
-        (car->CreateFixture(&cfd))->SetFcolor(255,0,0,1.0,0);
-        shape1.SetAsBox(rc*7.5f, rc*1.0f, rc*b2Vec2(0.0f,-2.0f), 0);
-        cfd.shape = &shape1;
-        (car->CreateFixture(&cfd))->SetFcolor(255,0,0,1.0,0);
-        //body curves
-        circle.m_radius = rc*2.5f;
-        circle.m_p=b2Vec2(rc*-2.5,0);      
-        cfd.shape = &circle;
-        (car->CreateFixture(&cfd))->SetFcolor(255,0,0,1.0);
-        circle.m_p=b2Vec2(rc*2.5,0);      
-        cfd.shape = &circle;
-        (car->CreateFixture(&cfd))->SetFcolor(255,0,0,1.0);
-        circle.m_radius = rc*1.25f;
-        circle.m_p=b2Vec2(rc*-6.25,rc*-1.25);
-        cfd.shape = &circle;
-        (car->CreateFixture(&cfd))->SetFcolor(255,0,0,1.0);
-        circle.m_p=b2Vec2(rc*6.25,rc*-1.25);
-        cfd.shape = &circle;
-        (car->CreateFixture(&cfd))->SetFcolor(255,0,0,1.0);    
-        //wheels
+        b2Body* body3;
+        //Revolving V platform
         {
-          b2Body* w1;
-          b2Body* w2;
-          b2CircleShape circle;
-          circle.m_radius = r*1.25;
-          b2FixtureDef ballfd;
-          ballfd.shape = &circle;
-          ballfd.density = 5.0f;
-          ballfd.friction = 0.0f;
-          ballfd.restitution = 0.0f;
-          b2BodyDef ballbd;
-          ballbd.type = b2_dynamicBody;       
-          
-          ballbd.position.Set(x+rc*(xc-4.75f), y+rc*(yc-4.0f));
-          w1 = m_world->CreateBody(&ballbd); 
-          (w1->CreateFixture(&ballfd))->SetFcolor(255,255,0,0.85);
-          ballbd.position.Set(x+rc*(xc+4.75f), y+rc*(yc-4.0f)); 
-          w2 = m_world->CreateBody(&ballbd);
-          (w2->CreateFixture(&ballfd))->SetFcolor(255,255,0,0.85);
-          //joints
-          b2DistanceJointDef jointDef;
-          jointDef.bodyA = w1;
-          jointDef.bodyB = car;
+          b2PolygonShape shape;
+          shape.SetAsBox(r*0.4f, r*8.0f);    
+          b2BodyDef bd;
+          bd.position.Set(x+r*12.0f, y+r*22.0f);
+          bd.type = b2_dynamicBody;
+          b2Body* body = m_world->CreateBody(&bd);
+          b2FixtureDef *fd = new b2FixtureDef;
+          fd->density = 10.0f;
+          fd->friction = 0.24f;
+          fd->shape = &shape;
+          (body->CreateFixture(fd))->SetFcolor(139,0,139,0.9);
+               
+          b2BodyDef bd2;
+          bd2.position.Set(x+r*12.0f, y+r*22.0f);
+          b2Body* bodyf = m_world->CreateBody(&bd2);
+
+          b2RevoluteJointDef jointDef;
+          jointDef.bodyA = body;
+          jointDef.bodyB = bodyf;
           jointDef.localAnchorA.Set(0,0);
-          jointDef.localAnchorB.Set(rc*-4.75f,rc*-3.0f);
-          jointDef.length = rc*0.8f;
-          jointDef.collideConnected = true;
-          jointDef.frequencyHz = 0.0f;
+          jointDef.localAnchorB.Set(0,0);
+          jointDef.collideConnected = false;
           m_world->CreateJoint(&jointDef);
 
-          jointDef.bodyA = w2;
-          jointDef.localAnchorB.Set(rc*4.75f,rc*-3.0f);
+          bd.position.Set(x+r*12.0f, y+r*30.0f);
+          b2Body* body2 = m_world->CreateBody(&bd);
+          shape.SetAsBox(r*0.4f, r*2.0f, r*b2Vec2(-1.4,-1.414), -b2_pi/4);
+          fd->shape = &shape;
+          (body2->CreateFixture(fd))->SetFcolor(139,0,139,0.9);
+
+          jointDef.bodyA = body;
+          jointDef.bodyB = body2;
+          jointDef.localAnchorA.Set(0,r*8.0f);
+          jointDef.localAnchorB.Set(0,r*0.0f);
           m_world->CreateJoint(&jointDef);
+
+          bd.position.Set(x+r*(12.0f-2*1.404), y+r*(30.0f-2*1.404));
+          body3 = m_world->CreateBody(&bd);
+          shape.SetAsBox(r*4.0f, r*0.4f, r*b2Vec2(-4.0f,0.0f), 0);
+          fd->shape = &shape;
+          (body3->CreateFixture(fd))->SetFcolor(139,0,139,0.9);
+          shape.SetAsBox(r*3.0f, r*2.0f, r*b2Vec2(-11.0f,0.0f), 0);
+          fd->shape = &shape;
+          (body3->CreateFixture(fd))->SetFcolor(139,0,139,1.0);
+
+          b2RopeJointDef jointDef2;
+          jointDef2.bodyA = body3;
+          jointDef2.bodyB = body2;
+          jointDef2.localAnchorA.Set(r*0.0f,r*0.0f);
+          jointDef2.localAnchorB.Set(r*-2.83f,r*-2.75f);
+          jointDef2.maxLength = r*0.05f;
+          m_world->CreateJoint(&jointDef2);
         }
-    }
   }
 }
